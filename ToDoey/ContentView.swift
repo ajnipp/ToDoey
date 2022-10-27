2b9edff580de4199cfe42919c518cf87dcd41ee2
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-10-27 16:47:54 -0400

    Initial Commit

---
+++ ToDoey/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDoey
+//
+//  Created by AlecNipp on 10/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoey/ToDoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoeyApp.swift
+//  ToDoey
+//
+//  Created by AlecNipp on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
